36b0caca4687fa54b6848150812e9b1d3427fcdb
Moaz Adrian <moazadrian@gmail.com>
2025-03-27 14:09:00 +0500

    Section Updates

--- app/page.js
+++ app/page.js
@@ -1,4 +1,8 @@
+import AvellanasOffer from './components/AvellanasOffer';
+import Footer from './components/Footer';
 import HeroSec from './components/HeroSec';
+import InfoSec from './components/InfoSec';
+import InstagramGrid from './components/InstagramGrid';
 import Navbar from './components/Navbar';
 
 export default function Home() {
@@ -7,6 +11,11 @@ export default function Home() {
    <>
    <Navbar />
    <HeroSec />
+   <InfoSec />
+   <AvellanasOffer />
+   {/* <h4>Reviews Section</h4> */}
+   <InstagramGrid />
+   <Footer />
    </>
   );
 }
